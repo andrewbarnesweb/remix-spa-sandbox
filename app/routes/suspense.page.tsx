9dfdd17eb53d90434bafec293ff9034f2c1a1532
kiliman <kiliman@gmail.com>
2024-03-29 08:42:31 -0400

    Fix defer to return promise, not awaited value

--- app/routes/suspense.page.tsx
+++ app/routes/suspense.page.tsx
@@ -2,27 +2,27 @@ import { Await, defer, useLoaderData } from '@remix-run/react';
 import { Suspense } from 'react';
 
 export const clientLoader = async () => {
-  const response: string = await new Promise((resolve) => {
+  const myPromise = new Promise<string>((resolve) => {
     setTimeout(() => {
       resolve(Math.random().toString());
     }, 2000);
   });
 
-  return defer({ response });
+  return defer({ myPromise });
 };
 
 function SearchResults() {
-  const { response } = useLoaderData<typeof clientLoader>();
+  const { myPromise } = useLoaderData<typeof clientLoader>();
 
   return (
     <div>
         Show me straight away
         <div>
             <Suspense fallback={<div>Loading locally</div>}>
-                <Await resolve={response}>
-                    {(response) => (
+                <Await resolve={myPromise}>
+                    {(randomValue) => (
                     <div>
-                        <h1>{response}</h1>
+                        <h1>{randomValue}</h1>
                     </div>
                     )}
                 </Await>
